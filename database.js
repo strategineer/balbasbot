bc7fcc80966341cfdcde316e71fb29fbdecb45a4
Bill Christian Tyros <me@btyros.com>
2020-04-23 02:26:46 -0400

    Fix db bug

--- database.js
+++ database.js
@@ -2,7 +2,6 @@ const util = require('./util.js')
 const sqlite3 = require('sqlite3').verbose();
 
 const TABLE_CHECK_SQL = "SELECT name FROM sqlite_master WHERE type='table' AND name=?";
-const TABLE_TRUNCATE_SQL = "TRUNCATE TABLE ?";
 
 const db = new sqlite3.Database('./balbasbot.db', (err) => {
     if (err) { console.error(err.message); }
@@ -18,7 +17,9 @@ function init() {
             db.get(TABLE_CHECK_SQL, [table.name], (err, row) => {
                 if (err) { console.error(err.message); }
                 if(!row) {
-                    db.run(`CREATE TABLE ${table.name}${table.sql}`)
+                    db.run(`CREATE TABLE ${table.name}${table.sql}`, [], (err) => {
+                        if (err) { console.error(err.message); }
+                    });
                 }
             });
         }
@@ -30,7 +31,7 @@ exports.init = init;
 function reset() {
     db.serialize(() => {
         for (table of util.data.database.tables) {
-            db.run(TABLE_TRUNCATE_SQL, [table.name], (err) => {
+            db.run(`DROP TABLE ${table.name}`, [], (err) => {
                 if (err) { console.error(err.message); }
             });
         }
